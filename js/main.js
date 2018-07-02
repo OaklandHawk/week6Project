35c4dffb6b103f383120adf15124c143e1201456
OaklandHawk <hawks93187@gmail.com>
2018-07-02 10:58:31 -0700

    second commit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
-main.js
+var firstName = prompt("What is your first name?")
+
+var lastName = prompt("What is your last name?")
+
+var userAge = prompt("How old are you?")
+
+greet();
+
+function greet () {
+
+	alert("Hello, " + firstName + " " + lastName + "!" + " You are " + (userAge * 12) + " months old and " + (userAge * 365) + " days old!");
+
+}
